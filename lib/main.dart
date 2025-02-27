7ffd0384476bd54489c8e02c709609cbebe4fb10
YEFRAMU <vinceumali81@gmail.com>
2025-02-27 22:15:11 +0800

    added TextButton on images

--- lib/main.dart
+++ lib/main.dart
@@ -55,12 +55,39 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Image(
-          image: AssetImage("images/dice1.png"),
-        ),
-      ],
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextButton(
+                onPressed: (){
+                  print('Left Dice Clicked!');
+                },
+                style: TextButton.styleFrom(
+                  padding: EdgeInsets.all(0)
+                ),
+                  child: Image.asset("images/dice1.png"),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextButton(
+                onPressed: (){
+                  print('Right Dice Clicked!');
+                },
+                style: TextButton.styleFrom(
+                  padding: EdgeInsets.zero
+                ),
+                  child: Image.asset("images/dice1.png"),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
